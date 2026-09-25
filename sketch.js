3c5cefdee98b0c1c84ab5cb18640ccf25df08d3c
Mokshagna Tirumala <mokshagna1229@gmail.com>
2026-09-25 19:49:53 +0530

    Another Particle Field

--- sketch.js
+++ sketch.js
@@ -3,25 +3,33 @@ const r = require("raylib");
 const window_width = 300;
 const window_height = 200;
 
-const detector_width = 50;
+const detector1_width = 20;
 const detector_height = window_height;
 let detector_start = 0;
 let detector_y = 0;
 
-const particle_field_start = 100;
-const particle_field_end = 150;
-const particle_field_y = 0;
-const particle_field_width = particle_field_end - particle_field_start;
-const particle_field_height = window_height;
 
-function overlap(range1_start, range1_end, range2_start, range2_end) {
-    range1 = range1_end - range1_start;
-    // range2 = range2_end - range2_start;
+const particle_field1_start = 100;
+const particle_field1_end = 150;
+const particle_field1_y = 0;
+const particle_field1_width = particle_field1_end - particle_field1_start;
+const particle_field1_height = window_height;
 
-    if (range1_end < range2_end + range1 - 1 && range1_end > range2_start - 1)
-        return r.RED;
-    else
-        return r.WHITE;
+const particle_field2_start = 200;
+const particle_field2_end = 205;
+const particle_field2_y = 0;
+const particle_field2_width = particle_field2_end - particle_field2_start;
+const particle_field2_height = window_height;
+
+
+function overlap(detector_start, detector_end, pf1_start, pf1_end, pf2_start, pf2_end) {
+    detectorWidth = detector_end - detector_start;
+    // range2 = pf1_end - pf1_start;
+
+    let pf1_overlap = detector_end < pf1_end + detectorWidth - 1 && detector_end > pf1_start - 1;
+    let pf2_overlap = detector_end < pf2_end + detectorWidth - 1 && detector_end > pf2_start - 1;
+
+    return pf1_overlap || pf2_overlap ? r.RED : r.WHITE;
 }
 
 function running() {
@@ -37,7 +45,7 @@ let flag = 0;
 
 function update() {
 
-    if (detector_start < window_width - detector_width && flag === 0) {
+    if (detector_start < window_width - detector1_width && flag === 0) {
         detector_start++;
     }
     else if (detector_start > 0) {
@@ -54,11 +62,13 @@ function draw() {
     r.BeginDrawing();
     r.ClearBackground(r.BLACK);
 
-    let detector_end = detector_start + detector_width;
-    let color = overlap(detector_start, detector_end, particle_field_start, particle_field_end);
-    r.DrawRectangle(particle_field_start, particle_field_y, particle_field_width, particle_field_height, r.BLUE);
+    let detector_end = detector_start + detector1_width;
+    let detector_color = overlap(detector_start, detector_end, particle_field1_start, particle_field1_end, particle_field2_start, particle_field2_end);
+    r.DrawRectangle(particle_field1_start, particle_field1_y, particle_field1_width, particle_field1_height, r.BLUE);
+    r.DrawRectangle(particle_field2_start, particle_field2_y, particle_field2_width, particle_field2_height, r.BLUE);
 
-    r.DrawRectangle(detector_start, detector_y, detector_width, detector_height, color);
+
+    r.DrawRectangle(detector_start, detector_y, detector1_width, detector_height, detector_color);
 
 
     r.EndDrawing();
