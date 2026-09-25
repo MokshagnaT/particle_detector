97f1fe1ff301d9b6896b9aed01473517496a40a0
Mokshagna Tirumala <mokshagna1229@gmail.com>
2026-09-25 18:33:54 +0530

    Build the scanner

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ const window_width = 300;
 const window_height = 200;
 const detector_width = 20;
 const detector_height = window_height;
+let detector_x = 0;
+let detector_y = 0;
+
 
 function running() {
     return !r.WindowShouldClose();
@@ -14,15 +17,28 @@ function setup() {
     r.SetTargetFPS(50);
 }
 
+let flag = 0;
+
 function update() {
-    // change the state
+
+    if (detector_x < window_width - detector_width && flag === 0) {
+        detector_x++;
+    }
+    else if (detector_x > 0) {
+        flag = 1;
+        detector_x--;
+        if (detector_x === 0) {
+            flag = 0;
+        }
+    }
+
 }
 
 function draw() {
     r.BeginDrawing();
     r.ClearBackground(r.BLACK);
 
-
+    r.DrawRectangle(detector_x, detector_y, detector_width, detector_height, r.WHITE);
 
     r.EndDrawing();
 }
